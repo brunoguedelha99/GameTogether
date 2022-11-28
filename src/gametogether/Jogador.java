7eb2460128433fef49479f4098ca398604a6c22a
brunoguedelha99 <brunoguedelha1999@gmail.com>
2022-11-28 14:47:27 +0000

    Complicado

--- src/gametogether/Jogador.java
+++ src/gametogether/Jogador.java
@@ -1,7 +1,56 @@
 package gametogether;
 
 public class Jogador {
-    String nome;
-    int idade;
-    String morada;
+    private String nome;
+    private int idade;
+    private String morada;
+    private String email;
+    private String password;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Jogador{" + "nome=" + nome + ", idade=" + idade + ", morada=" + morada + '}';
+    }
+    
 }
---
+++ src/gametogether/Jogo.java
@@ -0,0 +1,55 @@
+package gametogether;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Jogo {
+    private String nome;
+    private String local;
+    private String HoraDeJogo;
+    private Date DataDeJogo;
+    private ArrayList<Jogador> jogadores;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getHoraDeJogo() {
+        return HoraDeJogo;
+    }
+
+    public void setHoraDeJogo(String HoraDeJogo) {
+        this.HoraDeJogo = HoraDeJogo;
+    }
+
+    public Date getDataDeJogo() {
+        return DataDeJogo;
+    }
+
+    public void setDataDeJogo(Date DataDeJogo) {
+        this.DataDeJogo = DataDeJogo;
+    }
+
+    public ArrayList<Jogador> getJogadores() {
+        return jogadores;
+    }
+
+    public void add(Jogador player){
+        jogadores.add(player);
+    }
+    
+    
+    
+}
