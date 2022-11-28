2570b7729b0f91e62ebfd509ac8f56e2dd5b7a4c
brunoguedelha99 <brunoguedelha1999@gmail.com>
2022-11-28 09:22:31 +0000

    Getters and Setters e alteração do nome evento para jogo

--- src/gametogether/Jogador.java
+++ src/gametogether/Jogador.java
@@ -4,4 +4,35 @@ public class Jogador {
     String nome;
     int idade;
     String morada;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" + "nome=" + nome + ", idade=" + idade + ", morada=" + morada + '}';
+    }
+    
+    
 }
