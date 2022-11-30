adbfd49a68d0bcd6a2c155d7f54a60c4f9d50cf9
brunoguedelha99 <44906078+brunoguedelha99@users.noreply.github.com>
2022-11-30 11:55:15 +0000

    Update GameTogether.java

--- src/gametogether/GameTogether.java
+++ src/gametogether/GameTogether.java
@@ -1,12 +1,23 @@
 
 package gametogether;
 
+import java.util.ArrayList;
+
 
 public class GameTogether {
 
 
     public static void main(String[] args) {
 
+        ArrayList<Jogador> jogadores= new ArrayList();
+        Jogador Bruno = new Jogador("Bruno",22,"Lisboa","bruno@gmail.com","pass");
+        Jogador Diogo = new Jogador("Diogo",22,"Lisboa","email","pass");
+        
+        Jogo ABC =new Jogo("Futebol","Lisboa","19:00","1/12/22",jogadores);
+        ABC.add(Bruno);
+        ABC.add(Diogo);
+        
+        System.out.println(ABC.toString());
     }
     
 }
